d503abe89dc355602b82642eef4f1ecc451bc56f
Bryan Ochoa <bochoa314@gmail.com>
2024-08-09 19:38:33 -0400

    Initial Commit

---
+++ Fitapp/FitappApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitappApp.swift
+//  Fitapp
+//
+//  Created by Bryan Ochoa-Flores on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FitappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
